de24a4c96f6b0f209a97a21fca9d406adefaf067
Hakan GÜZEL <hakan-guzel@outlook.com>
2022-04-12 10:37:39 +0300

    Initial commit

---
+++ gRPCConsoleClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using gRPCServer.Protos;
+
+var data = new GetProductByIdModel { ProductId = 2 };
+var grpcChannel = GrpcChannel.ForAddress("https://localhost:7204");
+var client = new Product.ProductClient(grpcChannel);
+var response = await client.GetProductByIdAsync(data);
+Console.WriteLine(response);
+Console.ReadLine();
+using (var clientData = client.GetAllProducts(new GetAllProductsRequest()))
+{
+    while (await clientData.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
+    {
+        var thisProduct = clientData.ResponseStream.Current;
+        Console.WriteLine(thisProduct);
+    }
+}
+Console.ReadLine();
